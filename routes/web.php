1f4bf21acc73bd5a6affb26b0b6bc11afd1b4a8e
adriantpa <adrianltrc@yahoo.com>
2024-06-14 11:33:52 -0300

    first commit

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,32 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified',
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dash.index');
+//     })->name('dashboard');
+// });
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    ])->group(function () {
+        Route::get('/dashboard','App\Http\Controllers\DashboardController@index');
+    });
+
+
+
+Route::get('/dash/crud', function(){
+    return view('crud.index');
+});
+
+Route::get('/dash/crud/create', function(){
+    return view('crud.create');
 });
